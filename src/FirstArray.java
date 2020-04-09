110268a692168b83336bcfb56ef4a274efd93d68
acedrus <acedrus@mail.ru>
2020-04-09 16:58:25 +0300

    Initial commit

---
+++ src/FirstArray.java
@@ -0,0 +1,15 @@
+public class FirstArray {
+    public static void main(String... args) {
+        int[] firstArray = new int[900];
+        int[] secondArray = new int[firstArray.length];
+        for (int i = 0; i < firstArray.length; i++) {
+            firstArray[i] = i + 100;
+        }
+        for (int i = 0, j = firstArray.length - 1; i < firstArray.length; i++, j--) {
+            secondArray[j] = firstArray[i];
+        }
+        for (int i : secondArray) {
+            System.out.println(i);
+        }
+    }
+}
